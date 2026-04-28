ea9e2ea3f500e55b578cc7a79c6cb228a7379a92
Riyel10 <rakibulriyel1171@g,ail.com>
2026-04-28 20:32:42 +0600

    initial commit

--- test-api.js
+++
@@ -1,1 +0,0 @@
-fetch("http://localhost:5000/api/health").then(res => res.json()).then(console.log).catch(console.error);
--- test-login.js
+++
@@ -1,8 +0,0 @@
-fetch("http://localhost:5000/api/auth/login", {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({ email: "admin@example.com", password: "password" })
-}).then(async res => {
-  console.log(res.status);
-  console.log(await res.text());
-}).catch(console.error);
