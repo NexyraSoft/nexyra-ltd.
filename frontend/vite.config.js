6a96e4bce860a2747cbddf8332b26c570cd93af1
Riyel10 <rakibulriyel1171@g,ail.com>
2026-04-30 21:00:53 +0600

    initial commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,6 +9,7 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig(() => {
   return {
+    root: __dirname,
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
